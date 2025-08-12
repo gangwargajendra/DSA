8871e2a5885f98418f685e49a3e83eecada2efe3
Gajendra Gangwar <gangwargajendra8700@gmail.com>
2025-08-13 02:34:31 +0530

    Update L30WordLadder2nd.cpp

--- striverGraph/L30WordLadder2nd.cpp
+++ striverGraph/L30WordLadder2nd.cpp
@@ -2,8 +2,13 @@
 #include<vector>
 #include<queue>
 #include<set>
+#include<unordered_map>
+#include<string>
+#include<unordered_set>
+#include<algorithm>
 using namespace std;
 
+// normal way to sovle the word ladderII problem
 vector<vector<string>> wordLadderLength(string startWord , string targetWord , vector<string> &wordList){
     int n = wordList.size();
     int m = startWord.size();
@@ -14,7 +19,8 @@ vector<vector<string>> wordLadderLength(string startWord , string targetWord , v
     queue<vector<string>> q;
     q.push({startWord});
     mySet.erase(startWord);
-    set<string> tempSet; // it will use to store all new words that has been used in new level
+    set<string> crntLevelWord; // it will use to store all new words that has been used in new level
+    bool found = false;
 
     while(!q.empty()){
         int levelSize = q.size();
@@ -24,9 +30,6 @@ vector<vector<string>> wordLadderLength(string startWord , string targetWord , v
             q.pop();
             if(word == targetWord){
                 ans.push_back(sequence);
-            }
-            if(i == levelSize-1 && !ans.empty()){
-                break;
             }
 
             for(int j=0 ; j<m ; j++){
@@ -41,17 +44,83 @@ vector<vector<string>> wordLadderLength(string startWord , string targetWord , v
                         // }
                         newSequence.push_back(word);
                         q.push(newSequence);
-                        tempSet.insert(word);
+                        crntLevelWord.insert(word);
                     }
                 }
                 word[j] = original;
             }
         }
-        for(auto newWord : tempSet){
+        for(auto newWord : crntLevelWord){
             mySet.erase(newWord);
         }
-        tempSet.clear();
+        crntLevelWord.clear();
+        if(found) break;
     }
+    return ans;
+}
+
+// for leetcode, which will not give TLE
+
+void DFS(string startWord, string word, vector<string> &seq, vector<vector<string>> &ans, unordered_map<string, int> &map){
+    if(word == startWord){
+        reverse(seq.begin(), seq.end());
+        ans.push_back(seq);
+        reverse(seq.begin(), seq.end());
+        return;
+    }
+    int steps = map[word];
+    for(int i=0 ; i<word.size() ; i++){
+        char original = word[i];
+        for(char ch='a' ; ch<='z' ; ch++){
+            word[i] = ch;
+            if(map.find(word) != map.end() && map[word] + 1== steps){
+                seq.push_back(word);
+                DFS(startWord, word, seq, ans, map);
+                seq.pop_back();
+            }
+        }
+        word[i] = original;
+    }
+}
+
+
+vector<vector<string>> wordLadderIILeetcode(string startWord, string targetWord, vector<string> &wordList){
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    unordered_map<string, int> map;
+    queue<string> q;
+    q.push(startWord);
+    map[startWord] = 1;
+    int size = startWord.size();
+    st.erase(startWord);
+    // step 1: BFS to find the minimum steps to reach target word
+    while(!q.empty()){
+        string word = q.front();
+        int steps = map[word];
+        q.pop();
+        if(word == targetWord) break;
+        for(int i=0 ; i<size ; i++){
+            char original = word[i];
+            for(char ch='a' ; ch<='z' ; ch++){
+                word[i] = ch;
+                if(st.count(word)){
+                    q.push(word);
+                    st.erase(word);
+                    map[word] = steps + 1;
+                }
+            }
+            word[i] = original;
+        }
+    }
+    
+    // step 2: Backtrack to find all the paths using DFS
+    vector<vector<string>> ans;
+    
+    if(map.find(targetWord) != map.end()){
+        vector<string> seq;
+        seq.push_back(targetWord);
+        DFS(startWord, targetWord, seq, ans, map);
+    }
+
     return ans;
 }
 
@@ -70,7 +139,7 @@ int main(){
     cout << "Enter start and target word : ";
     cin >> startWord >> targetWord;
 
-    vector<vector<string>> ans = wordLadderLength(startWord , targetWord ,wordList);
+    vector<vector<string>> ans = wordLadderIILeetcode(startWord , targetWord ,wordList);
 
     cout << "Answer of given problem : "<< endl;
     for(int i=0 ; i<ans.size() ; i++){
